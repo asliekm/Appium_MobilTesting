6c346c5b6b543fc906497b10dc7445a702bfc15a
Nevzat <nevzatcelik151907@gmail.com>
2024-03-05 12:51:28 +0100

    third lesson

--- src/test/java/tests/KiwiApp.java
+++ src/test/java/tests/KiwiApp.java
@@ -1,5 +1,13 @@
 package tests;
 
+import org.testng.annotations.Test;
+import utils.Driver;
+
 public class KiwiApp {
 
+
+    @Test
+    public void kiwiAppTest(){
+
+    }
 }
--- src/test/java/tests/apkYukleme.java
+++ src/test/java/tests/apkYukleme.java
@@ -21,7 +21,7 @@ public class apkYukleme {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\Appium_T127\\Apps\\arabam.com_5.1.6_Apkpure.apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\Appium_T127\\Apps\\Kiwi.com - Book Cheap Flights_2023.14.0_Apkpure.apk");
         // UiAutomator 2 otomasyon ismi sadece android 6 dan yuksek olan android sistemleri icin calisir
         // UiAutomator otomasyon ismi sadece android 6 ve 6 dan dusuk olan android sistemleri icin calisir
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
